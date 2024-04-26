e99c6f26de89b48d4b01b2259638e9dc2910d424
cass <casswoo@gmail.com>
2024-04-27 07:27:48 +0900

    pyscript

--- cass/urls.py
+++ cass/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("hello", views.hello, name="hello"),
     path("test/", views.test, name="test"),
-    path("test2/<int:n>/", views.test2, name="test2")
+    path("test2/<int:n>/", views.test2, name="test2"),
+    path("test3/", views.test3, name="test3_base"),
+    path("test3/<str:name>/", views.test3, name="test3_name"),
+    path("test4/", views.test4, name="test4_base"),
+    path("test4/<str:object_type>/", views.test4_type, name="test4_object_type"),
+    path("test5/", views.test5, name="test5_base"),
+    path("test6/", views.test6, name="test6_base"),
 ]
--- cass/views.py
+++ cass/views.py
@@ -26,3 +26,24 @@ def test2(request, n):
     primes = [p * 10 for p in primes]
 
     return render(request, "test2.html", {"primes": primes})
+
+def test3(request, name=None):
+
+    if name:
+        name = f"이름은 {name}입니다."
+    else:
+        name = "이름이 없습니다."
+
+    return render(request, "test3.html", {"name": name})
+
+def test4(request):
+    return render(request, "test4.html")
+
+def test4_type(request, object_type):
+    return render(request, "test4.html", {"object_type": object_type})
+
+def test5(request):
+    return render(request, "test5.html")
+
+def test6(request):
+    return render(request, "test6.html")
